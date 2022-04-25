b53b6cc0d93d35972a3b3f5068d58e60ef507f2a
lilleyt09 <lilleyt09@gmail.com>
2022-04-25 08:48:42 -0400

    added navbar and page frameworks

---
+++ react-website/src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react";
+  
+const About = () => {
+  return (
+    <div>
+      <h1>
+        --Insert information about L and J here--
+      </h1>
+    </div>
+  );
+};
+  
+export default About;
